322622083c58109bad1fcee6aa88594c77f9a7bd
Mateusz Kurek <m.kurek1994@gmail.com>
2024-11-17 23:16:16 +0100

    Add new task form

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,25 +1,48 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TextInput,
+  Button,
+} from "react-native";
 import React, { useState } from "react";
 import TaskListItem from "./TaskListItem";
 
 export interface Task {
-    description: string;
+  description: string;
 }
 
 const TaskList = () => {
-    const [tasks, setTasks] = useState<Task[]>([
-        {description: 'First item'},
-        {description: 'Second item'},
-    ]);
+  const [tasks, setTasks] = useState<Task[]>([
+    { description: "First item" },
+    { description: "Second item" },
+  ]);
+
+  const [newTask, setNewTask] = useState("");
+
+  const createTask = () => {
+    setTasks([...tasks, { description: newTask }]);
+    setNewTask("");
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Todo</Text>
 
-      <FlatList data={tasks} renderItem={({item}) => (
-        <TaskListItem task={item} />
-      )} contentContainerStyle={{ gap: 5 }}/>
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => <TaskListItem task={item} />}
+        contentContainerStyle={{ gap: 5 }}
+      />
 
-      
+      <TextInput
+        placeholder="New task"
+        placeholderTextColor="gray"
+        style={styles.input}
+        value={newTask}
+        onChangeText={setNewTask}
+      />
+      <Button title="Add task" onPress={createTask} />
     </View>
   );
 };
@@ -30,12 +53,19 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#101112",
     padding: 10,
-    borderRadius: 5
+    borderRadius: 5,
+    gap: 5,
   },
   text: {
-    color: '#FFF',
-    fontWeight: 'bold',
+    color: "#FFF",
+    fontWeight: "bold",
     fontSize: 20,
-    marginVertical: 10
-  }
+    marginVertical: 10,
+  },
+  input: {
+    color: "#FFF",
+    padding: 15,
+    backgroundColor: "#1D2125",
+    borderRadius: 5,
+  },
 });
